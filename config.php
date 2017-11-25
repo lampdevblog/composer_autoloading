ca7f2c338b6a5f511d9362fafc32d92b5cfd7833
Sergey Gerashchenko <gerra.lampdev@gmail.com>
2017-11-25 11:50:00 +0200

    initial commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+require_once __DIR__ . '/vendor/autoload.php';
+
+define("HOST", "localhost");
+define("USER", "root");
+define("PASSWORD", "");
+define("DBNAME", "guestbook");
+define("CHARSET", "utf8");
+define("SALT", "webDEVblog");
